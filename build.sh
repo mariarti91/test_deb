f5e1ec67b91a8df6fabfa712c5e13b824ba6e307
Dmitriy Nikiforov <d.nikiforov@ion.team>
2020-06-15 18:27:30 +0700

    fix mistakes

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 dpkg-buildpackage -us -uc -d
-mv ../test-service-*.deb ./
+mv ../test-service_*.deb ./
 rm ../deb-test_0.1.*
-rm test-service-*-dbgsym*
+rm ../test-service-dbgsym*.deb
